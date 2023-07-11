a8e2c4ca45933cb069070435551ac72a908c1c6b
NamPE286 <nambuihung654@gmail.com>
2023-07-11 16:31:08 +0700

    init

---
+++ index.ts
@@ -0,0 +1,58 @@
+import express from 'express'
+import dotenv from 'dotenv';
+import fs from 'fs'
+import path from 'path'
+import { checkUser, setCaching } from './middleware';
+
+dotenv.config()
+
+const app = express()
+
+function* walkSync(dir: string): Generator<string> {
+    const files = fs.readdirSync(dir, { withFileTypes: true });
+    for (const file of files) {
+        if (file.isDirectory()) {
+            yield* walkSync(path.join(dir, file.name));
+        } else {
+            yield path.join(dir, file.name);
+        }
+    }
+}
+
+for (const filePath of walkSync('./src')) {
+    var reqPath: string = './' + filePath
+        .split('\\')
+        .join('/')
+        .slice(0, -3)
+    var route: string = '/' + reqPath
+        .split('/')
+        .slice(2, -1)
+        .join('/')
+        .replaceAll('[', ':')
+        .replaceAll(']', '')
+        .replaceAll('{', ':')
+        .replaceAll('}', '?')
+    import(reqPath).then(fn => {
+        if (reqPath.endsWith('GET', reqPath.length)) {
+            app.get(route, setCaching, fn.main)
+        }
+        if (reqPath.endsWith('POST', reqPath.length)) {
+            app.post(route, checkUser, fn.main)
+        }
+        if (reqPath.endsWith('PUT', reqPath.length)) {
+            app.put(route, checkUser, fn.main)
+        }
+        if (reqPath.endsWith('DELETE', reqPath.length)) {
+            app.delete(route, checkUser, fn.main)
+        }
+
+    })
+    console.log(`Loaded path ${reqPath} to route ${route}`)
+}
+
+app.listen(
+    5050,
+    () => {
+        console.log(`Server running on http://localhost:5050`)
+    }
+)
---
+++ middleware.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from 'express';
+
+export function checkUser(req: Request, res: Response, next: NextFunction): void {
+    next()
+}
+
+export function setCaching(req: Request, res: Response, next: NextFunction): void {
+    res.set('Cache-Control', 'public, s-maxage=10, stale-while-revalidate=3600')
+    next()
+}
