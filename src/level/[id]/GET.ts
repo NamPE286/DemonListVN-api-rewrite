e802d7b41eab365c87967c27b99025822bf35dbf
NamPE286 <nambuihung654@gmail.com>
2023-07-13 00:45:18 +0700

    handle 404

--- src/level/[id]/GET.ts
+++ src/level/[id]/GET.ts
@@ -4,5 +4,11 @@ import Level from '@classes/Level';
 export default async function (req: Request, res: Response) {
     const { id } = req.params
     const level = new Level(parseInt(id));
-    res.send(JSON.stringify(await level.init()))
+    level.init()
+        .then(data => {
+            res.send(JSON.stringify(data))
+        })
+        .catch(() => {
+            res.status(404).send()
+        })
 }
