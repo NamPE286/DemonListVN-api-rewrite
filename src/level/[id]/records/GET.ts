4dc493ef3ca104e27101c95e762c44df5da6329d
NamPE286 <nambuihung654@gmail.com>
2023-07-14 12:16:45 +0700

    /level/[id]/records GET docs

--- src/level/[id]/records/GET.ts
+++ src/level/[id]/records/GET.ts
@@ -1,6 +1,29 @@
 import { Request, Response } from 'express';
 import Level from '@classes/Level';
 
+/**
+ * @openapi
+ * /level/{id}/records:
+ *  get:
+ *      tags:
+ *      - Level
+ *      summary: Get a level's records by ID
+ *      parameters:
+ *        - name: id
+ *          in: path
+ *          description: The ID of the level
+ *          required: true
+ *          schema:
+ *              type: integer
+ *      responses:
+ *          200:
+ *              description: Sucessful operation
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *          404:
+ *              description: Invalid ID or level does not exist
+ */
 export default async function (req: Request, res: Response) {
     const { id } = req.params
     const level = new Level(parseInt(id));
