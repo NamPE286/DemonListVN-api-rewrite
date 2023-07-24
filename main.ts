bd1560c158d316b229ad050de84e94f8313966d3
NamPE286 <nambuihung654@gmail.com>
2023-07-24 10:33:20 +0700

    disable docs

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 import app from './app.ts';
-import swaggerDocs from '@utils/swagger.ts'
+// import swaggerDocs from '@utils/swagger.ts'
 
 app.listen(
     5050,
     () => {
         console.log(`Server running on http://localhost:5050`)
-        swaggerDocs(app, 5050)
+        // swaggerDocs(app, 5050)
     }
 )
