10efb98365a150a70273b196bffadd8efa5cc58b
NamPE286 <nambuihung654@gmail.com>
2023-07-24 11:08:58 +0700

    update

--- db.ts
+++ db.ts
@@ -1,4 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 import { SUPABASE_API_URL, SUPABASE_API_KEY } from "./env.ts";
 
-export const supabase = createClient(SUPABASE_API_URL, SUPABASE_API_KEY)
+export const supabase = createClient(SUPABASE_API_URL, SUPABASE_API_KEY, {
+    auth: {
+        persistSession: false
+    }
+})
